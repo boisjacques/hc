fc863ecedcaceb333884dcd2a69c4c5005914a3e
Manuel Kieweg <mail@manuelkieweg.de>
2020-07-21 23:03:06 +0200

    Multi Device Bridge

--- accessory/mqttAccessory.go
+++ accessory/mqttAccessory.go
@@ -1,9 +1,9 @@
 package accessory
 
 import (
+	"github.com/boisjacques/hc/log"
 	"github.com/boisjacques/hc/service"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"log"
 	"strconv"
 	"strings"
 )
@@ -21,52 +21,54 @@ type MqttAccessory struct {
 func (a *MqttAccessory) HandleMessage(msg mqtt.Message) {
 	topic := msg.Topic()
 	topic = strings.Split(topic, "/")[2]
+	log.Debug.Printf("Split topic:\t%v", topic)
 	stringified := string(msg.Payload())
-	if value, err := strconv.ParseFloat(stringified, 32); err != nil {
-		switch topic {
-		case "temperature":
-			a.handleTemperature(value)
-		case "humidity":
-			a.handleHumidity(value)
-		}
+	value, err := strconv.ParseFloat(stringified, 32)
+	if err != nil {
+		log.Debug.Fatalf("Error handling Message %v\nMessage String: %v\n", msg.Payload(), stringified)
+	}
+	switch topic {
+	case "temperature":
+		a.handleTemperature(value)
+	case "humidity":
+		a.handleHumidity(value)
 	}
 }
 
 func (a *MqttAccessory) handleTemperature(temperature float64) {
 	a.TempSensor.CurrentTemperature.SetValue(temperature)
-	log.Printf("Temperature set to %f\n", temperature)
+	log.Info.Printf("Temperature set to %f\n", temperature)
 }
 
 func (a *MqttAccessory) handleHumidity(humidity float64) {
-	a.TempSensor.CurrentTemperature.SetValue(humidity)
-	log.Printf("Temperature set to %f\n", humidity)
+	a.HumiditySensor.CurrentRelativeHumidity.SetValue(humidity)
+	log.Info.Printf("Humidity set to %f\n", humidity)
 }
 
 func NewMqttAccessory(info Info) *MqttAccessory {
 	acc := MqttAccessory{}
-	typ, _ := strconv.Atoi(info.Model)
-	acc.Accessory = New(info, AccessoryType(typ))
-	if typ == 5 {
+	acc.Accessory = New(info, info.DeviceType)
+	if info.DeviceType == 5 {
 		acc.Light = service.NewColoredLightbulb()
 		acc.Light.Hue.SetValue(0)
 		acc.Light.Saturation.SetValue(0)
 		acc.Light.Brightness.SetValue(0)
 		acc.AddService(acc.Light.Service)
-	} else if typ == 9 {
+		log.Debug.Printf("Topic:\t%v\tService:\t%v\n", acc.Type, acc.Light.Type)
+	} else if info.DeviceType == 9 {
 		acc.TempSensor = service.NewTemperatureSensor()
 		acc.TempSensor.CurrentTemperature.SetValue(20)
 		acc.TempSensor.CurrentTemperature.SetMinValue(-50)
 		acc.TempSensor.CurrentTemperature.SetMaxValue(50)
 		acc.TempSensor.CurrentTemperature.SetStepValue(0.1)
 		acc.AddService(acc.TempSensor.Service)
-		/*
-			acc.HumiditySensor = service.NewHumiditySensor()
-			acc.HumiditySensor.CurrentRelativeHumidity.SetValue(50)
-			acc.HumiditySensor.CurrentRelativeHumidity.SetMinValue(0)
-			acc.HumiditySensor.CurrentRelativeHumidity.SetMaxValue(100)
-			acc.HumiditySensor.CurrentRelativeHumidity.SetStepValue(0.1)
-			acc.Accessory.AddService(acc.HumiditySensor.Service)
-		*/
+		log.Debug.Printf("Topic:\t%v\tService:\t%v\n", acc.Type, acc.TempSensor.Type)
+		acc.HumiditySensor = service.NewHumiditySensor()
+		acc.HumiditySensor.CurrentRelativeHumidity.SetValue(50)
+		acc.HumiditySensor.CurrentRelativeHumidity.SetMinValue(0)
+		acc.HumiditySensor.CurrentRelativeHumidity.SetMaxValue(100)
+		acc.HumiditySensor.CurrentRelativeHumidity.SetStepValue(0.1)
+		acc.Accessory.AddService(acc.HumiditySensor.Service)
 	}
 	return &acc
 }
--- mqtt/dispatcher.go
+++ mqtt/dispatcher.go
@@ -2,6 +2,7 @@ package mqtt
 
 import (
 	"github.com/boisjacques/hc/accessory"
+	"github.com/boisjacques/hc/log"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
@@ -25,6 +26,7 @@ func (d *Dispatcher) AddTopic(topic string, acc accessory.MqttAccessory) {
 	d.deviceMap[topic] = acc
 	go func() {
 		d.client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+			log.Debug.Println("Waiting for message...")
 			d.rx <- msg
 		})
 	}()
@@ -33,6 +35,7 @@ func (d *Dispatcher) AddTopic(topic string, acc accessory.MqttAccessory) {
 func (d *Dispatcher) Listen() {
 	for {
 		msg := <-d.rx
+		log.Debug.Printf("Topic:\t%v\nPayload:\t%v", msg.Topic(), msg.Payload())
 		mqttAccessory := d.deviceMap[msg.Topic()]
 		mqttAccessory.HandleMessage(msg)
 	}
