f63601ef9fadd79c20e0a766442d7fb13c3f1c43
Manuel Kieweg <mail@manuelkieweg.de>
2020-07-21 03:05:01 +0200

    not working. changing number of transports

---
+++ hkinventory/hkinventory.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"github.com/boisjacques/hc"
+	"github.com/boisjacques/hc/accessory"
+	"github.com/boisjacques/hc/mqtt"
+	"log"
+	"sync"
+)
+
+func main() {
+	username := flag.String("username", "mqttuser", "mqtt user")
+	password := flag.String("password", "mqttpassword", "mqtt password")
+	uri := flag.String("uri", "tcp://192.168.2.250:1883", "mqtt broker uri")
+	cid := flag.String("cid", "FreeBSD", "ClientID")
+	flag.Parse()
+
+	transports := make([]hc.Transport, 0)
+	bridge := *mqtt.NewMQTTBridge()
+	var wg sync.WaitGroup
+
+	// TODO: Write json config with accessories	}
+	infos := []accessory.Info{
+		{
+			Name:         "Manucave",
+			Manufacturer: "HoChiMinh Flowerpower Enterprises",
+			DeviceType:   9,
+			Topics: []string{
+				"/home/manu/temperature",
+				"/home/manu/humidity",
+			},
+		},
+		{
+			Name:         "Mancave",
+			Manufacturer: "HoChiMinh Flowerpower Enterprises",
+			DeviceType:   9,
+			Topics: []string{
+				"/home/mancave/temperature",
+				"/home/mancave/humidity",
+			},
+		},
+		{
+			Name:         "Basement",
+			Manufacturer: "HoChiMinh Flowerpower Enterprises",
+			DeviceType:   9,
+			Topics: []string{
+				"/home/basement/temperature",
+				"/home/basement/humidity",
+			},
+		},
+	}
+
+	client := bridge.Register(*username, *password, *cid, *uri)
+	dispatcher := mqtt.NewDispatcher(client)
+
+	for _, info := range infos {
+		acc := accessory.NewMqttAccessory(info)
+		for _, topic := range info.Topics {
+			dispatcher.AddTopic(topic, *acc)
+		}
+		var tCast hc.Transport = t
+		transports = append(transports, tCast)
+	}
+
+	t, err := hc.NewIPTransport(hc.Config{Pin: "11223344"}, acc.Accessory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dispatcher.Listen()
+
+	hc.OnTermination(func() {
+		<-t.Stop()
+	})
+	wg.Add(1)
+	go t.Start(&wg)
+	wg.Wait()
+}
